2a6e7530217f2404d87c6adbed5060bed8b93e06
Johan Hedman <johan@dreamsland.de>
2026-02-20 15:19:15 +0100

    delete page in url

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -12,7 +12,6 @@ $routes = [
     '' => [$class, 'index'],
     'hallo' => [$class, 'hallo'],
     'user' => [$class, 'user'],
-    'page' => [$class, 'page'],
     'bootstrap' => [$class, 'bootstrap'],
     'installation' => [$class, 'installation']
 ];
